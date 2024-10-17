08919729d2cebca6af5da09e386333624c44c458
UgwuObed <vjazzyvictor@gmail.com>
2024-10-17 13:45:01 +0100

    fest

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,37 @@
 // require('ts-node/register');
-require('dotenv/config.js')
+require('dotenv/config.js');
 
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DATABASE_URL } = process.env;
 
 module.exports = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
-  logging: false,
-  migrationStorageTableName: "migrations",
-  pool: {
-    max: 2,
-    min: 0,
-    acquire: 3000,
-    idle: 0
-  },
-  define: {
-    freezeTableName: true
-  }
+  ...(DATABASE_URL
+    ? {
+        use_env_variable: 'DATABASE_URL', 
+        dialect: 'postgres',  
+        dialectOptions: {
+          ssl: {
+            require: true,  
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'mysql', 
+        logging: false,
+        migrationStorageTableName: 'migrations',
+        pool: {
+          max: 2,
+          min: 0,
+          acquire: 3000,
+          idle: 0,
+        },
+        define: {
+          freezeTableName: true,
+        },
+      }),
 };
