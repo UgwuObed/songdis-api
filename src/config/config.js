27a670287ee50f7baa37c9ccb666b3bb05c0f9c4
UgwuObed <vjazzyvictor@gmail.com>
2024-10-17 14:49:56 +0100

    Fixed module import errors

--- src/config/config.js
+++ src/config/config.js
@@ -1,48 +1,37 @@
-// // require('ts-node/register');
-// require('dotenv/config.js');
+// require('ts-node/register');
+require('dotenv/config.js');
 
-// const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DATABASE_URL } = process.env;
-
-// module.exports = {
-//   ...(DATABASE_URL
-//     ? {
-//         use_env_variable: 'DATABASE_URL', 
-//         dialect: 'postgres',  
-//         dialectOptions: {
-//           ssl: {
-//             require: true,  
-//             rejectUnauthorized: false,
-//           },
-//         },
-//       }
-//     : {
-//         username: DB_USERNAME,
-//         password: DB_PASSWORD,
-//         database: DB_NAME,
-//         host: DB_HOST,
-//         port: DB_PORT,
-//         dialect: 'mysql', 
-//         logging: false,
-//         migrationStorageTableName: 'migrations',
-//         pool: {
-//           max: 2,
-//           min: 0,
-//           acquire: 3000,
-//           idle: 0,
-//         },
-//         define: {
-//           freezeTableName: true,
-//         },
-//       }),
-// };
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DATABASE_URL } = process.env;
 
 module.exports = {
-  use_env_variable: 'DATABASE_URL',
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+  ...(DATABASE_URL
+    ? {
+        use_env_variable: 'DATABASE_URL', 
+        dialect: 'postgres',  
+        dialectOptions: {
+          ssl: {
+            require: true,  
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'mysql', 
+        logging: false,
+        migrationStorageTableName: 'migrations',
+        pool: {
+          max: 2,
+          min: 0,
+          acquire: 3000,
+          idle: 0,
+        },
+        define: {
+          freezeTableName: true,
+        },
+      }),
 };
