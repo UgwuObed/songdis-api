0c32d67f69a55d26af50a97dfe3ff634f1268c88
UgwuObed <vjazzyvictor@gmail.com>
2024-10-17 14:31:50 +0100

    Fixed module import errors

--- src/datasources/cache.ts
+++ src/datasources/cache.ts
@@ -1,21 +1,21 @@
-// import { Logger } from "../libs";
-// import env from "../config/env";
-// import { createClient } from "redis";
+import { Logger } from "../libs";
+import env from "../config/env";
+import { createClient } from "redis";
 
 
-// const redisClient = env.NODE_ENV === "dev" ? createClient({
-//     socket: {
-//         host: env.REDIS_HOST,
-//         port: parseInt(env.REDIS_PORT)
-//     }
-// }) : createClient({
-//     url: env.REDIS_HOST
-// })
+const redisClient = env.NODE_ENV === "dev" ? createClient({
+    socket: {
+        host: env.REDIS_HOST,
+        port: parseInt(env.REDIS_PORT)
+    }
+}) : createClient({
+    url: env.REDIS_HOST
+})
 
 
-// redisClient
-//     .connect()
-//     .then(() => Logger.info("redis connected successfully"))
-//     .catch((error) => Logger.error(error));
+redisClient
+    .connect()
+    .then(() => Logger.info("redis connected successfully"))
+    .catch((error) => Logger.error(error));
 
-// export default redisClient;
+export default redisClient;
